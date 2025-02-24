fd1c04d6840e70bbba5816ebeb72985081272296
Arghadeep-hub <mallickarghadeep1@gmai.com>
2025-02-24 16:51:22 +0530

    profile updated

--- components/templates/Home/HeroSection.tsx
+++ components/templates/Home/HeroSection.tsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 import React from "react";
 import { BlurImage } from "@/components/ui/BluerImage";
+import { useAppSelector } from "@/redux/hooks";
 
 const TabSection = dynamic(
   () => import("@/components/templates/Home/TabSection")
@@ -8,6 +9,8 @@ const TabSection = dynamic(
 const Container = dynamic(() => import("@/components/shared/Container"));
 
 function HeroSection() {
+  const user = useAppSelector((state) => state.user);
+
   return (
     <div id="hero" className="relative">
       <Container className="pt-10 flex justify-start items-start gap-x-2">
@@ -16,14 +19,14 @@ function HeroSection() {
           {/* Top Heading */}
           <div className="mt-[2%] flex justify-start items-center gap-x-3">
             <BlurImage
-              src={"/images/profile.jpg"}
+              src={user?.image}
               alt="profile"
-              width={100}
-              height={100}
+              width={200}
+              height={200}
               className="size-[75px] object-cover rounded-full border-[3px] border-yellow-400"
             />
             <div className="py-1 px-1 leading-relaxed">
-              <h2 className="font-medium">Hello, Hồng Oanh!</h2>
+              <h2 className="font-medium capitalize">Hello, {user.name}!</h2>
               <p className="text-p1-r">
                 Here are the details to help you plan your next holiday
               </p>
